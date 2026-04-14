eb2e771928f66ca65681c6b0520568891ce6f3e0
Dario Dias <dariodias97@gmail.com>
2026-04-14 14:08:06 +0100

    Refactor: add semicolon after namespace declaration

--- HotBevMachine/RoundButton.cs
+++ HotBevMachine/RoundButton.cs
@@ -1,32 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing.Drawing2D;
 
-namespace HotBevMachine
+namespace HotBevMachine;
+
+public class RoundButton : Button
 {
-    public class RoundButton : Button
+    protected override void OnPaint(PaintEventArgs pevent)
     {
-        protected override void OnPaint(PaintEventArgs pevent)
+        GraphicsPath p = new GraphicsPath();
+        p.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+        Region = new Region(p);
+        base.OnPaint(pevent);
+
+        FlatAppearance.BorderColor = BackColor;
+
+        if (Name == "rbt1E" || Name == "rbt2E")
         {
-            GraphicsPath p = new GraphicsPath();
-            p.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
-            Region = new Region(p);
-            base.OnPaint(pevent);
-            
-            FlatAppearance.BorderColor = BackColor;
-
-            if (Name == "rbt1E" || Name == "rbt2E")
-            {
-                Pen pen = new(ForeColor, (Font.Size + 8) / 2);
-                pen.Alignment = PenAlignment.Inset;
-                pevent.Graphics.DrawPath(pen, p);
-                pen.Dispose();
-            }
-
-            p.Dispose();
+            Pen pen = new(ForeColor, (Font.Size + 8) / 2);
+            pen.Alignment = PenAlignment.Inset;
+            pevent.Graphics.DrawPath(pen, p);
+            pen.Dispose();
         }
+
+        p.Dispose();
     }
 }
